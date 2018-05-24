895659661e1e508f1f5d4d115435cf546803d824
WusztPC <WusztPC@b7f660ee-b0ca-5c42-9875-4a590c7d3ce5>
2018-05-24 21:59:31 +0000

    Created other empty projects and moved everything to one solution.
    git-svn-id: https://wusztserver.ddns.net/svn/Main/BugsReporter@85 b7f660ee-b0ca-5c42-9875-4a590c7d3ce5

--- BugsReporterClient/Properties/AssemblyInfo.cs
+++ BugsReporterClient/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BugsReporterServer")]
+[assembly: AssemblyTitle("BugsReporterClient")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BugsReporterServer")]
+[assembly: AssemblyProduct("BugsReporterClient")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bd90a3a5-26c9-4241-ace3-a1e4a96702ef")]
+[assembly: Guid("5c8152d1-0e1b-4f91-a7f1-1dc6884774c7")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BugsReporterModel/Issue.cs
@@ -0,0 +1,19 @@
+﻿namespace BugsReporterModel
+{
+    public class Issue
+    {
+        public int ID { get; set; }
+        public IssueType Type { get; set;}
+
+        public string Stack { get; set; }
+
+        public string UserInfo { get; set; }
+    }
+
+    public enum IssueType
+    {
+        Unknown = 0,
+        Bug = 1,
+        Crash = 2
+    }
+}
